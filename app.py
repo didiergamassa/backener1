c4b969d8c37196e8c22659ed243744bfd7d9abcf
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-08 18:50:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, send_file
 import pandas as pd
 import numpy as np
-import calendar
-from calendar import monthrange
+
 
 
 # Monthly consumption data
@@ -13,21 +12,7 @@ gas_data = pd.DataFrame({'Month': ['january','february','march','april','may','j
 water_data = pd.DataFrame({'Month': ['january','february','march','april','may','june','july','august','september','october','november','december'],
                            'Consumption': [75, 75, 77, 78, 77, 80, 82, 85, 82, 81, 86, 75]})
 
-# Function to generate random daily consumption data for each month
-def generate_daily_consumption(months, mean_consumption, std_dev):
-    data = {}
-    for month in months:
-        year = 2023
-        num_days = monthrange(year, list(calendar.month_name).index(month.capitalize()))[1]  # Get the number of days in the month
-        data[month] = np.random.normal(mean_consumption, std_dev, num_days).astype(int)
-    return data
 
-
-
-# Calculate daily consumption
-electricity_daily = {key: value.tolist() for key, value in generate_daily_consumption(electricity_data['Month'], mean_consumption=4000, std_dev=1000).items()}
-gas_daily = {key: value.tolist() for key, value in generate_daily_consumption(gas_data['Month'], mean_consumption=2000, std_dev=500).items()}
-water_daily = {key: value.tolist() for key, value in generate_daily_consumption(water_data['Month'], mean_consumption=50, std_dev=10).items()}
 
 
 app = Flask(__name__)
@@ -48,10 +33,6 @@ def get_gas_data():
 def get_water_data():
     return jsonify(water_data.to_dict(orient='records'))
 
-@app.route("/consumption_data")
-def get_daily_consumption_data():
-    consumption_data = {'Electricity': electricity_daily,'Gas':gas_daily,'Water': water_daily}
-    return jsonify(consumption_data)
 
 
 if   __name__ == "__main__":
